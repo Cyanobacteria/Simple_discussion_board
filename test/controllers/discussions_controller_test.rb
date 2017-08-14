65e34ae8edb417718ade3396ecc809338bd4f948
cyanobacteria <vodomelkor@gmail.com>
2017-08-14 19:30:13 +0800

    要先學會模擬使用者登入才可以開始測試各個頁面

--- test/controllers/discussions_controller_test.rb
+++ test/controllers/discussions_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class DiscussionsControllerTest < ActionDispatch::IntegrationTest
+  def set_params
+    @params = {:id => "1", :post_id => "1"}
+  end
 
-
-
+  test "should get edit page" do
+    get edit_post_discussion_path(id: "1", post_id: "1")
+    assert_response :success 
+    
+  end
     
 
 
