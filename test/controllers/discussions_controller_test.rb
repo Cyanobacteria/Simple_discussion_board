85c080a2b2867b60fcb65114569288d349e3f978
cyanobacteria <vodomelkor@gmail.com>
2017-08-14 18:32:37 +0800

    刪除scaffold自動生成的test

--- test/controllers/discussions_controller_test.rb
+++ test/controllers/discussions_controller_test.rb
@@ -1,19 +1,12 @@
 require 'test_helper'
 
 class DiscussionsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @discussion = discussions(:one)
-  end
 
 
 
     
 
 
-  test "should get edit" do
-    get edit_discussion_url(@discussion)
-    assert_response :success
-  end
 
   
  
