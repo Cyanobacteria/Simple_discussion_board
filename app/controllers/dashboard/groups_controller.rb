995a6a2b0648bba1c5222dca56b1a1ab9776dad9
cyanobacteria <vodomelkor@gmail.com>
2017-08-28 16:20:19 +0800

    設定好dashboard/groups_controller各項功能骨架準備開始實作

--- app/controllers/dashboard/groups_controller.rb
+++ app/controllers/dashboard/groups_controller.rb
@@ -1,9 +1,34 @@
 class Dashboard::GroupsController < Dashboard::DashboardController
    
   def index
+   @groups = Group.all
+   #顯示所有group 並印出相應的show edit destroy 
+   #有按鈕連結到new來新增 group  
+  end
+
+  def show
+    #show出對應的group 並顯示所有該group底下的post
+   #一樣要能夠新增post並有show edit destroy
+  end
+
+  def new
+   #表單 新增頁面
+  end
+
+  def create
+   #新增動作本身
+  end
+
+  def edit
    
-     
-    
+  end
+
+  def update
+
+  end
+
+  def destroy
+
   end
 
 end
