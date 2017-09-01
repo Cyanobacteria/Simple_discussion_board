258720c7a54293c3c7560cbf94116e2a97cbc954
cyanobacteria <vodomelkor@gmail.com>
2017-09-01 23:57:46 +0800

    整理helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,36 +1,4 @@
 module ApplicationHelper
-=begin
-  def last_updated_post_of_group(id)
-    group = Group.find(id).posts
-    if group.count == 0
-      return "目前沒有文章"
-    else
-      group.order('updated_at').last.title
-    end 
-  end
-
-  def show_time_group(id)
-    time = Time.now
-    group = Group.find(id).posts
-    if group.count == 0
-      return "目前沒有文章"
-    else
-     #寫一個把超過24小時換成天 30天以上換成月+天 
-      time_distance = "#{((time - group.order('updated_at').last.updated_at) / 3600).round(1)} 小時前"
-      time_updated = "#{group.order('updated_at').last.updated_at}"
-      output = time_updated + "(" + time_distance + ")"
-      output
-    end 
-  end
-  def last_created_post_of_group(id)
-    group = Group.find(id).posts
-    if group.count == 0
-      return "目前沒有文章"
-    else
-      group.order('created_at').last.title
-    end 
-  end
-=end
 
   def sort_tool(id , option1, option2 )
    # id 對應 group本身的id
